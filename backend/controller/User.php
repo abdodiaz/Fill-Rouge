9a76c0bdbe08e65bc9ad10119481338589fa0a0d
dazia <abdelbarredazia@gmail.com>
2021-07-27 11:05:44 +0100

    update

--- backend/controller/User.php
+++ backend/controller/User.php
@@ -4,51 +4,38 @@ class User
 {
     function index()
     {
-      $data = json_decode(file_get_contents("php://input"));
-
-      $ref=new UserModel();
-
-      if(!empty($data->Reference))
-      {
-          // $ref->reference=$data->reference;
-
-          $value=$ref->GetAll($data->Reference);
-
-          if($value)
-          {
-          // Ici la création a fonctionné
-          // On envoie un code 201
-          http_response_code(201);
-          echo json_encode(["message" => "ref  a été effectué","user_ref"=>$value["Reference"]]);
-          }else
-          {
-              // Ici la création n'a pas fonctionné
-              // On envoie un code 503
-              http_response_code(503);
-              echo json_encode(["message" => "ref n'a pas été effectué "]);
-          }
-      }else
-      {
-          // On gère l'erreur
-          http_response_code(405);
-          echo json_encode(["message" => "La méthode n'est pas autorisée"]);
-      }
+        $ref = new UserModel();
+        $value = $ref->GetAll();
+        foreach ($value as $val) {
+            if ($val) {
+                // Ici la création a fonctionné
+                // On envoie un code 201
+                http_response_code(201);
+                echo json_encode(["id" => $val["Id"],"name" => $val["Name"],"name" => $val["Name"], "name" => $val["Name"]]);
+            } else {
+                // Ici la création n'a pas fonctionné
+                // On envoie un code 503
+                http_response_code(503);
+                echo json_encode(["message" => "ref n'a pas été effectué "]);
+            }
+        }
     }
-    function create(){
-      $data = json_decode(file_get_contents("php://input"));
-          $obj = new UserModel();
-        //generate
-        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        $result = '';
-        for ($i = 0; $i < 5; $i++)
-        $result .= $characters[mt_rand(0, 61)];
-        $obj->setCin($data->cin);
-        $obj->setReference($obj->setCin($data->cin).$result);
-        $obj->setNom($data->nom);
-        $obj->setPrenom($data->prenom);
-        $obj->setEmail($data->email);
-        $obj->setTel($data->tel);
-        $obj->setAge($data->age);
-        $obj->Insert();
+    function create()
+    {
+        $data = json_decode(file_get_contents("php://input"));
+        //   $obj = new UserModel();
+        // //generate
+        // $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        // $result = '';
+        // for ($i = 0; $i < 5; $i++)
+        // $result .= $characters[mt_rand(0, 61)];
+        // $obj->setCin($data->cin);
+        // $obj->setReference($obj->setCin($data->cin).$result);
+        // $obj->setNom($data->nom);
+        // $obj->setPrenom($data->prenom);
+        // $obj->setEmail($data->email);
+        // $obj->setTel($data->tel);
+        // $obj->setAge($data->age);
+        // $obj->Insert();
     }
 }
--- backend/model/UserModel.php
+++ backend/model/UserModel.php
@@ -2,57 +2,16 @@
 require __DIR__ . "/../Config.php";
 
 class UserModel{
-    public $con;
-    private  $reference;
-    Private $cin;
-    private  $nom;
-    private  $prenom;
-    private  $age;
-    private  $email;
-    private  $tel;
-
-    public function setReference($reference){
-        $this->reference = $reference;
-    }
-    public function setCin($cin){
-        $this->cin = $cin;
-     }
-
-    public function setNom($nom){
-        $this->nom = $nom;
-     }
-
-     public function setAge($age){
-        $this->age = $age;
-     }
-
-     public function setPrenom($prenom){
-        $this->prenom = $prenom;
-     }
-
-     public function setEmail($email){
-        $this->email = $email;
-     }
-
-     public function setTel($tel){
-        $this->tel = $tel;
-     }
-    
+  
     function __construct()
     {
         $this->con=new Config();
     }
-    function GetAll($refence)
+    function GetAll()
     { 
-        $selects="SELECT * FROM user where Reference = '$refence'";
+        $selects="SELECT * FROM user";
          $z= $this->con->connect($selects);
         return $z->fetchAll(PDO::FETCH_ASSOC); 
     }
     
-    function Insert()
-    {
-    $insert='INSERT INTO `user`(`Referance`, `Cin`, `Nom`, `Prenom`, `Tele`, `Age`, `Email`) VALUES ("'.$this->reference.'","'.$this->cin.'","'.$this->nom.'","'.$this->prenom.'","'.$this->tel.'",'.$this->age.',"'.$this->email.'")';
-    $this->con->connect($insert);    
-    }
- 
 }
